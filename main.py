f6441bad95dc030b2724ddad5747418e61a79407
Lawrence Najim <najimlawr@myvuw.ac.nz>
2023-01-15 14:06:30 +1300

    Added random number generation

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # Python version of the HigherLower game.
+import random
+
+
+# Generate number between 1 and 20 to use in game
+def generate_num():
+    return random.randint(1, 20)
+
 
 if __name__ == '__main__':
     print("empty")

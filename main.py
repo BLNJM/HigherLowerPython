3c656892bebf03109f6eaf88b51ba75c47e8b321
Lawrence Najim <najimlawr@myvuw.ac.nz>
2023-01-15 14:16:29 +1300

    Let user make a guess, limit guess to only valid inputs

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ def generate_num():
 
 
 if __name__ == '__main__':
-    print("empty")
+    # generate first number to show user
+    first_num = generate_num()
+
+    # get guess from the user
+    while True:
+        try:
+            guess = int(input('The first number is ' + str(first_num) + '. Please enter a number between 1-20: '))
+
+            # only accept guess if it is within accepted range
+            if guess in range(1, 21):
+                print("Your choice was: ", guess)
+                break
+        # only accept guess if it is an integer
+        except ValueError:
+            print('Invalid response, please enter a number between 1-20')

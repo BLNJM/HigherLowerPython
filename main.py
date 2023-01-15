9499648322f165354eda4d5a9b8acd9399eda33f
Lawrence Najim <najimlawr@myvuw.ac.nz>
2023-01-15 14:37:12 +1300

    Reformat guess input logic and added results logic

--- main.py
+++ main.py
@@ -8,21 +8,28 @@ def generate_num():
 
 
 if __name__ == '__main__':
+    guess = ""
+
     # generate first number to show user
     first_num = generate_num()
 
     # get guess from the user
     while True:
-        try:
-            guess = input('Number is ' + str(first_num) + '. Will the next be "higher" or "lower"? (between 1-20):')
+        guess = input('Number is ' + str(first_num) + '. Will the next be "higher" or "lower"? (between 1-20):')
 
-            # only accept guess if it a valid guess
-            if guess.lower() == 'higher' or guess.lower() == 'lower':
-                break
+        # only accept guess if it is a valid guess
+        if guess.casefold() == 'higher' or guess.casefold() == 'lower':
+            break
         # only accept guess if it is an integer
-        except ValueError:
-            print('Invalid response, please enter either "higher" or "lower"')
+        print('Invalid response, please enter either "higher" or "lower"')
 
-        second_num = generate_num()
+    second_num = generate_num()
+    print('The first number is ' + str(first_num) + ' and the second number is ' + str(second_num))
 
-
+    # Results logic
+    if first_num > second_num and guess.casefold() == 'lower':
+        print('You were correct!')
+    elif first_num < second_num and guess.casefold() == 'higher':
+        print('You were correct!')
+    else:
+        print("Sorry, but you were incorrect")
